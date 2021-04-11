6c664e7923201367e6ef58cc4f864f77bf630d1b
mahesh jamdade <maheshmn121@gmail.com>
2021-04-11 17:44:18 +0530

    Initial Commit

---
+++ Shared/weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appApp.swift
+//  Shared
+//
+//  Created by Mahesh Jamdade on 11/04/21.
+//
+
+import SwiftUI
+
+@main
+struct weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
